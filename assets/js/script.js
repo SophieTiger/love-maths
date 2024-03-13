54d665184401e8ba94447614f5812f4d30d11bdc
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-03-13 11:24:31 +0000

    Clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,18 +12,18 @@ document.addEventListener("DOMContentLoaded", function () {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        })
+        });
     }
 
     document.getElementById("answer-box").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             checkAnswer();
         }
-    })
+    });
     
     runGame("addition");
 
-})
+});
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
@@ -133,9 +133,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
     operand1 = operand1 * operand2;
-    
-    document.getElementById('operand1').textContent = operand1 
-    document.getElementById('operand2').textContent = operand2
+
+    document.getElementById('operand1').textContent = operand1; 
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
     
 }
